6aa4567a137ac1b28b6dee6b2a4d3e5afc05d587
priya <pgunter@maxpayload.com>
2021-05-04 19:35:34 -0400

    moving average filter added

--- table_creation.m
+++ table_creation.m
@@ -7,13 +7,6 @@ awa_z = aw_data(:,4);
 awa_abs = aw_data(:,5);
 classaw = repmat(mode,[1 length(awa_z)]);
 classaw = classaw(:);
-
-%smooth data
-awa_x = smooth_filter(awa_x);
-awa_y = smooth_filter(awa_y);
-awa_z = smooth_filter(awa_z);
-awa_abs = smooth_filter(awa_abs);
-
 awalkingdata=table(awa_x,awa_y,awa_z,awa_abs,classaw);
 awalkingdata.Properties.VariableNames = ["A_x","A_y","A_z","A_abs","Motion"];
 
