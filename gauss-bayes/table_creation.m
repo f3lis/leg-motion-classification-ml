a50ae98a3ed1463a347478111ea5725e1aa390ca
priya <pgunter@maxpayload.com>
2021-05-08 12:11:21 -0400

    added smoother back in

--- gauss-bayes/table_creation.m
+++ gauss-bayes/table_creation.m
@@ -5,6 +5,12 @@ awa_x = aw_data(:,2);
 awa_y = aw_data(:,3);
 awa_z = aw_data(:,4);
 awa_abs = aw_data(:,5);
+
+awa_x=smooth_filter(awa_x);
+awa_y=smooth_filter(awa_y);
+awa_z=smooth_filter(awa_z);
+awa_abs=smooth_filter(awa_abs);
+
 classaw = repmat(mode,[1 length(awa_z)]);
 classaw = classaw(:);
 awalkingdata=table(awa_x,awa_y,awa_z,awa_abs,classaw);
